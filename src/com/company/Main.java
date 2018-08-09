14751bdd1c7e3653d703cdaf26a1f8875a1a374e
cauldy <38020603+cauldy@users.noreply.github.com>
2018-08-09 19:11:30 +0200

    komentarz 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Integer result = divide(1,2);
-        System.out.println("Konrad Czmanskis result: " + result);
+        double result = divide(1,2);
+        System.out.println("Result: " + result);
     }
-    public static Integer divide(Integer a, Integer b){
-        Integer c;
+    public static double divide(double a, double b){
+        double c;
         c = a/b;
-        return c+c;
+        return c;
     }
 
 }
