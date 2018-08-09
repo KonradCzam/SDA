6b7dff11e46d2fffeef61dd45b4c0979cafec6f9
Jakub Maciejewski <maciejewski1000@gmail.com>
2018-08-09 19:57:50 +0200

    zmiany 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,27 @@ public class Main {
 
     public static void main(String[] args)
     {
-
-        System.out.println("Konrad Czmanskis result: ");
+        double result = divide(1,2);
+        int result1 = subtraction(6, 4);
+        System.out.println("Konrad Czmanskis result: " + result);
+    }
+    public static Double divide(double a, double b){
+        Double c;
+        c = a/b;
+        return c;
     }
 
+
+    public Integer multiply(Integer a, Integer b){
+        Integer c;
+        c = a*b;
+        return c;
+    }
+    public static Integer subtraction( int a, int b){
+
+        int c;
+        c = a - b;
+
+        return c;
+    }
 }
