b402158abdeb8e6170d288f6193cd809715b2234
Lukasz Kroll <gredloc10@gmail.com>
2018-08-09 18:46:18 +0200

    moj commit nie wiem o co chodzi xd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
     {
 
         System.out.println("Konrad Czmanskis result: ");
+        System.out.println(multiply(5, 21));
+
+    }
+    public static Integer multiply(Integer a, Integer b){
+        return a * b;
+
+
     }
 
 }
