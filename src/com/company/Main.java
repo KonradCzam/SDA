ddd40c5eb19855204e66160e1957f7f8fb21c886
KonradCzam <konradcz.sluzbowy@gmail.com>
2018-08-09 15:34:46 +0200

    Main class cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,8 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Integer result = add(1,2);
-        System.out.println("Konrad Czmanskis result: " + result.toString());
-    }
-    public static Integer add(Integer a, Integer b){
-        Integer c;
-        c = a+b;
-        c = a+b;
-        c = a+b;
 
-        return c;
+        System.out.println("Konrad Czmanskis result: ");
     }
+
 }
