a0a1213ffb9cc502c5c51c3247376eb58fe9e579
Ada Michocka <amichocka@wp.pl>
2018-08-09 18:33:04 +0200

    no message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
     {
 
         System.out.println("Konrad Czmanskis result: ");
+
+
+    }
+    public static Integer multiply(Integer a, Integer b){
+        Integer d = a*b;
+        return d;
+
     }
 
 }
