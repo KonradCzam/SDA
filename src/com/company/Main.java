14531a4e0ae0f255b8c1bfa50dc806c209dfc530
Szymsi <sienkiewicz.szymon@gmail.com>
2018-08-09 20:07:58 +0200

    Odejmowanie Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        public static Integer multiply(Integer a, Integer b, Integer c){
-            Integer c = a * b;
-            return c;
-
-
-            System.out.println("Konrad Czmanskis result: " + a * b);
         }
+
+    public static Integer multiply(Integer a, Integer b, Integer c){
+        Integer c = a * b;
+        return c;
+
+    }
+
+    public static Integer subtraction (Integer a, Integer b, Integer c) {
+        Integer c = a - b;
+        return c;
     }
 }
