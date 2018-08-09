c2608402c2d905c2997c946c3d980a88b0ad2e55
Jakub Maciejewski <maciejewski1000@gmail.com>
2018-08-09 20:57:00 +0200

    ostatni

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,38 @@ package com.company;
 
 public class Main {
 
+    private static double wynik;
+    private double a;
+    private double b;
+
+
     public static void main(String[] args)
     {
+        System.out.println(dodawanie(2,5));
+        System.out.println(mnozenie(3,4));
+        System.out.println(odejmowanie(5,3));
+        System.out.println(dzielenie(4,2));
 
-        System.out.println("Konrad Czmanskis result: ");
+    }
+    public static double dodawanie(double a, double b){
+         wynik = a + b;
+
+        return wynik;
+    }
+    public static double odejmowanie(double a, double b){
+        wynik = a - b;
+
+        return wynik;
+    }
+    public static double mnozenie(double a, double b){
+        wynik = a * b;
+
+        return wynik;
     }
 
+    public static double dzielenie(double a, double b){
+        wynik = a / b;
+
+        return wynik;
+    }
 }
