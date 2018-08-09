c2fc4776cb4dabc0ac0aa7eeeda722608a9227e4
KonradCzam <konradcz.sluzbowy@gmail.com>
2018-08-09 15:38:34 +0200

    This totally works. For Sure!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-
-        System.out.println("Konrad Czmanskis result: ");
+        Integer result = divide(1,2);
+        System.out.println("Konrad Czmanskis result: " + result);
+    }
+    public static Integer divide(Integer a, Integer b){
+        Integer c;
+        c = a/b;
+        return c+c;
     }
 
 }
